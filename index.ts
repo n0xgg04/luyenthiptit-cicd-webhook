cfc9041cbe395a6304f4dde2a67b586c1d282844
n0xgg04 <dulieu.vblc@gmail.com>
2024-04-05 16:08:36 +0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import bodyParser from "body-parser";
+import {execSync} from "child_process"
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/webhook", (req, res) => {
+    if (req.headers['user-agent'] !== 'Docker-Hub-Sec') {
+        return res.sendStatus(401);
+    }
+    execSync("docker rm -f luyenthiptit-web && docker pull n0xgg04/luyenthiptit:latest && docker-compose up -d")
+    res.sendStatus(200)
+})
+
+app.listen(3344).listen(() => {
+    console.log("listening...")
+})
