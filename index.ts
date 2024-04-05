f288ebbf6e770dc12f2679a5748ec984af0596c7
n0xgg04 <dulieu.vblc@gmail.com>
2024-04-05 16:30:26 +0700

    a

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import bodyParser from "body-parser";
 import {execSync} from "child_process"
+import morgan from "morgan"
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"))
 
 app.post("/webhook", (req, res) => {
     if (req.headers['user-agent'] !== 'Docker-Hub-Sec') {
