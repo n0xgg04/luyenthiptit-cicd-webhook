621049d489d3bb119ae8de2a1e5a74d3df31a12c
n0xgg04 <dulieu.vblc@gmail.com>
2024-04-05 22:57:31 +0700

    a

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 import bodyParser from "body-parser";
 import {execSync} from "child_process"
 import morgan from "morgan"
@@ -8,11 +8,13 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"))
 
-app.post("/webhook", (req, res) => {
+app.post("/webhook", (req: Request, res: Response) => {
     if (req.headers['user-agent'] !== 'Docker-Hub-Sec') {
         return res.sendStatus(401);
     }
-    execSync("docker rm -f luyenthiptit-web && docker pull n0xgg04/luyenthiptit:latest && docker-compose up -d")
+    const a = execSync("cd /root/luyen-thi-ptit-golang && git pull && docker compose down web && docker pull n0xgg04/luyenthiptit:latest && docker compose up web -d",{
+    })
+    console.log(a.toString())
     res.sendStatus(200)
 })
 
